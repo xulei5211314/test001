464ccb451dee1f6181cede4d19bcc9a6ed68039a
经理 <manager@163.com>
2021-10-10 00:00:33 +0800

    删除12.py

--- test/login.py
+++ test/login.py
@@ -9,23 +9,6 @@ num1=10
 num3=300
 "swadsadddddddddddddddd" \
 "dasdddddddddddddddddddddddddddddddddddddd" \
-"sdaaaaaaaaaaaaaadddddddddddddddddddddddddddddddddddddddd" \
-"dsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
-"dasssssssssssssssssssssssssssss" \
-"asd" \
-"" \
-"" \
-"" \
-"" \
-"" \
-"" \
-"" \
-"" \
-"" \
-"" \
-"" \
-"" \
-"" \
-"dsadasdddddddddddd" \
+"sdddddddddd" \
 "dsaaaaaaaaaaaaaaaa" \
 "asddddddddddd"
